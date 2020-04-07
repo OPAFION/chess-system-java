1cf5310f00e3fab285a04958e52f9f2a12204bd3
Daniel Oliveira <daniel13.oliveira.do@gmail.com>
2020-04-07 16:47:28 -0300

    BoardException and defensive programming

--- src/chess/pieces/Rook.java
+++ src/chess/pieces/Rook.java
@@ -9,11 +9,7 @@ public class Rook extends ChessPiece {
     public Rook(Color color, Board board) {
         super(color, board);
     }
-
-//    public Rook(Board board, Color color) {
-//        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-//    }
-    
+   
     @Override
     public String toString(){
         return "R";
