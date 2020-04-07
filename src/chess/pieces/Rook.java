b488c0a3e9b3d8f2e3484f1de373a9ae6fba8789
Daniel Oliveira <daniel13.oliveira.do@gmail.com>
2020-04-07 16:27:32 -0300

    Placing pieces on the board

---
+++ src/chess/pieces/Rook.java
@@ -0,0 +1,21 @@
+package chess.pieces;
+
+import boardgame.Board;
+import chess.ChessPiece;
+import chess.Color;
+
+public class Rook extends ChessPiece {
+
+    public Rook(Color color, Board board) {
+        super(color, board);
+    }
+
+//    public Rook(Board board, Color color) {
+//        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+//    }
+    
+    @Override
+    public String toString(){
+        return "R";
+    }
+}
